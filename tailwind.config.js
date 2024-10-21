5b5737d6bcd9280437627ddc1c69786a2f41414d
ari afandi <ari.affndii.com>
2024-10-21 21:34:02 +0700

    memperbaiki struktur folder

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  content: ["./src/**/*.{html,js}", "./index.html"],
+  content: ["./src/**/*.{html,js}", "./index.html", "./pages/**/*.{html,js}"],
   theme: {
     extend: {
       fontFamily: {
